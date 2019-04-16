040aff8304e6e7cd79cd63dade425552e5d32e88
Onechezzz <maximilian.afanasyev@gmail.com>
2019-04-16 20:51:44 +0300

    Changing Weather.js for better looking.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,10 @@ const Weather = props => (
   <div className="infoWeath">
   { props.city &&
     <div>
-    <p>Местоположение:{props.city},{props.country}</p>
-    <p>Температура:{props.temp}</p>
-    <p>Давление:{props.pressure}мм.</p>
-    <p>Заход солнца:{props.sunset}</p>
+    <p>Местоположение: {props.city}, {props.country}</p>
+    <p>Температура: {props.temp}</p>
+    <p>Давление: {props.pressure} мм.</p>
+    <p>Заход солнца: {props.sunset}</p>
   </div>
   }
     <p className="error">{props.error}</p>
